8b275624e45943d38ee67c917a819baae8b55592
SaberValkyrie <93904326+SaberValkyrie@users.noreply.github.com>
2022-04-01 17:13:28 +0700

    Initial commit

---
+++ src/Race.java
@@ -0,0 +1,17 @@
+package src;
+
+public class Race {
+    public static void main(String a[]) {
+        RaceCar[] cars = new  RaceCar[5];
+        cars[0] = new RaceCar(10, "Marrio");
+        cars[1] = new RaceCar(10, "Songoku");
+        cars[2] = new RaceCar(10, "Herman");
+        cars[3] = new RaceCar(10, "Doraemon");
+        cars[4] = new RaceCar(10, "Hoang Phi Hong");
+
+        for (int i=0; i <5;i++)
+            cars[i].run();
+        }
+    }
+
+
---
+++ src/Thread.java
@@ -0,0 +1,6 @@
+package src;
+
+public class Thread {
+    protected static void sleep(long round) {
+    }
+}
